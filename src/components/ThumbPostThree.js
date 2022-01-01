8d86104e905e57cf229b3e13c428558bca7594a3
Irwin <irwinngo@gmail.com>
2022-01-01 01:30:32 -0500

    Pls work longer BROO

--- src/components/ThumbPostThree.js
+++ src/components/ThumbPostThree.js
@@ -2,16 +2,30 @@ const postWidth = 250, postHeight = postWidth;
 
 const ThumbPostThree = ( { title, date, text, author, image } ) => {
     return (
-        <div>
+        <div style={postContainerStyle}>
             <img style={imageStyle} src={image} alt=""/>
+            <div>
+                <p>{title}</p>
+            </div>
+            <div>
+                <p>{author}</p>
+            </div>
+            <div>
+                <p>{date}</p>
+            </div>
 
         </div>
     )
 }
 
 const imageStyle = {
+    position: 'relative',
     width: postWidth,
     height: postHeight*3/4,
+}
+
+const postContainerStyle = {
+    textAlign: 'center',
 }
 
 export default ThumbPostThree
